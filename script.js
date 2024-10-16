1e48cefe624f0ef2a2acde29eb680225c5d1e0ba
ingriddeandradelopes <163137409+ingriddeandradelopes@users.noreply.github.com>
2024-10-16 08:50:36 -0300

    2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function alterarCirculo(){
-    cont circulo=document.getElementById('circulo');
+    const circulo=document.getElementById('circulo');
 
     const corAleatoria ='#'+
-    Math.floor(mart.random()*16777215).toString(16);
+    Math.floor(Math.random()*16777215).toString(16);
 
     const novoTamanho=Math.floor(Math.random()*(200-50+1))+50;
 
     circulo.style.backgroundColor = corAleatoria;
     circulo.style.width = novoTamanho + 'px';
-    circulo.stule.height = novoTamanho + 'px';
+    circulo.style.height = novoTamanho + 'px';
 }
